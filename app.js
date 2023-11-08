c1d68a53c6fe53962d38333d67047713098b40dc
maxife36 <maxife36@gmail.com>
2023-11-07 22:00:07 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const pathPublic = path.resolve("public")
+
+app.use(express.static(pathPublic))
+
+const PORT = 3001
+
+//---Paths---
+
+const pathIndex = path.resolve("views", "index.html") 
+const pathLogin = path.resolve("views", "login.html") 
+const pathProductCart = path.resolve("views", "productCart.html") 
+const pathProductDetail = path.resolve("views", "productDetail.html") 
+const pathRegister = path.resolve("views", "register.html") 
+
+
+//----Peticiones CRUD----
+
+app.get("/", (req,res) => res.sendFile(pathIndex))
+app.get("/login", (req,res) => res.sendFile(pathLogin))
+app.get("/product-cart", (req,res) => res.sendFile(pathProductCart))
+app.get("/product-detail", (req,res) => res.sendFile(pathProductDetail))
+app.get("/register", (req,res) => res.sendFile(pathRegister))
+
+
+app.listen(PORT, () =>{
+    console.log(`Se conecto Correctamnete a 
+    http://localhost:${PORT}`);
+})
