3e85519daa26aac4cb2972d82d43ef95a2651dbc
FrancoDelfino <francogiovannidelfino@gmail.com>
2023-12-15 16:51:55 -0300

    agrego product create

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,5 +5,8 @@ const controllers = require('../API-Controllers/productsControllers.js')
 
 router.get("/detail", controllers.productoDetail)
 router.get("/edit", controllers.productEdit)
+router.post("/edit", controllers.productStore)
+
+
 
 module.exports = router;
