b72ce7a43d3be0b52be1b401abd82aaa667294b4
candearismendi <candearis25@gmail.com>
2024-01-22 17:43:19 -0300

    cambios en register

--- src/API-Controllers/usersControllers.js
+++ src/API-Controllers/usersControllers.js
@@ -1,5 +1,40 @@
+const bcrypt = require('bcrypt')
 const controllers = {
-    productCart: (req,res) => res.render("productCart.ejs")
+    productCart: (req,res) => res.render("productCart.ejs"),
+    showForm: (req, res) => {
+        res.render('/register.ejs');
+      },
+    processRegister: (req, res) => {
+        const { fullName, userEmail, userBirthday, userAdress, userName, password, confirmPassword } = req.body;
+        // const avatarPath = req.file.path;
+        const newUser = {
+            fullName: fullName,
+            email: userEmail,
+            adress: userAdress,
+            userName: userName,
+            password: bcrypt.hash(password, 10)
+        }
+        bcrypt.hash(password, 10, (err, hash) => {
+            if (err) throw err;
+    
+            const newUser = {
+                username,
+                password: hash,
+                avatar: avatarPath
+            };
+            fs.readFile('usuarios.json', 'utf8', (err, data) => {
+                if (err) throw err;
+    
+                const usuarios = JSON.parse(data);
+                usuarios.push(newUser);
+    
+                fs.writeFile('usuarios.json', JSON.stringify(usuarios), (err) => {
+                    if (err) throw err;
+                    res.send('¡Registro exitoso!');
+                });
+            });
+        });
+    }
 }
 
 module.exports = controllers;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,4 +5,9 @@ const controllers = require("../API-Controllers/usersControllers.js")
 
 router.get("/cart", controllers.productCart);
 
+
+router.get('/register', controllers.showForm);
+
+router.post('/register-form',  controllers.processRegister);
+
 module.exports = router;
